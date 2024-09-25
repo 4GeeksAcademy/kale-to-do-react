5a3cc92e529329d59bd557cc2b7a345f85bb939e
KaleCordero <kalecascante61@gmail.com>
2024-09-25 05:03:25 +0000

    entrega

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [todoList, setTodoList] = useState("")
+	const [valor, setValor] = useState("")
+
+	const agregarTarea = (e) => {
+		if (e.key === 'Enter' && valor.trim()) {
+			setTodoList([...todoList, valor]);
+			setValor('');
+		}
+	}
+
+	const borrarTarea = (index) => {
+		setTodoList(todoList.filter((_, i) => i !== index));
+	}
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="text-center container w-25 justify-content-center">
+			<h1>todos</h1>
+			<div className="principal">
+				<ul className="list-group">
+					<input className="titulo" type="text" value={valor} onChange={(e) => setValor(e.target.value)}
+						onKeyDown={agregarTarea} placeholder="Whats need to be done?" />
+					{todoList.length === 0 ? (
+						<li className="list-group-item disabled" aria-disabled="true">No hay tareas</li>
+					) : (
+						todoList.map((todoList, index) =>
+						(
+							<li className="list-group-item list-group-item-light d-flex justify-content-between" key={{ index }}>
+								{todoList} <button className="btn btn-outline-danger" onClick={() => borrarTarea(index)}>X</button>
+							</li>
+						))
+					)
+					}
+					<li className="list-group-item text-start"><p>{todoList.length} item left</p></li>
+					<li className="list-group-item primero"></li>
+					<li className="list-group-item segundo"></li>
+				</ul>
+			</div>
 		</div>
 	);
 };
